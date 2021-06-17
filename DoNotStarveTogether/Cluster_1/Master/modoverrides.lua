80429266249e264f6f1a8d1c88e79bf57f20364f
root <root@ip-172-31-10-140.ec2.internal>
2021-06-17 12:59:23 +0000

    init

---
+++ DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,11 @@
+return {
+	["workshop-345692228"] = {enable = true},
+	["workshop-347079953"] = {enable = true},
+	["workshop-363112314"] = {enable = true},
+	["workshop-375859599"] = {enable = true},
+	["workshop-378160973"] = {enable = true},
+	["workshop-458587300"] = {enable = true},
+	["workshop-501385076"] = {enable = true},
+	["workshop-1271089343"] = {enable = true},
+	["workshop-949808360"] = {enable = true}
+}
---
+++ DoNotStarveTogether/modoverrides.lua
@@ -0,0 +1,11 @@
+return {
+	["workshop-345692228"] = { enabled = true},
+	["workshop-347079953"] = { enabled = true},
+	["workshop-363112314"] = { enabled = true},
+	["workshop-375859599"] = { enabled = true},
+	["workshop-378160973"] = { enabled = true},
+	["workshop-458587300"] = { enabled = true},
+	["workshop-501385076"] = { enabled = true},
+	["workshop-1271089343"] = { enabled = true},
+	["workshop-949808360"] = { enabled = true}
+}
